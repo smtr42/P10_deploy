20c9710a00063c166fe2f6960f89e50d626031af
Teiva <t.simonnet@mf2i.fr>
2020-10-15 18:27:50 +0200

    Test Travis

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from .views import detail, fav, save, sub_list
 app_name = "products"
 urlpatterns = [
     path("", sub_list, name="sub_list"),
-    path("save", save, name="save"),
+    path("save", save, name="sauve"),
     path("fav", fav, name="fav"),
     path("detail", detail, name="detail"),
 ]
